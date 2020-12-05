bbffb6a6a753d21eef2c2eb3774c62c7a7cc9be1
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-12-05 19:28:00 +0530

    Add all the basic navigation bar components to nav-slide-left

---
+++ Nav-Slide-Left/app.js
@@ -0,0 +1,14 @@
+const sidepane = document.querySelector(".sidepane");
+sidepane.classList.remove("expanded");
+
+document.querySelector(".burger").addEventListener('click',()=>{
+    const top = document.querySelector("#top");
+    const middle = document.querySelector("#middle");
+    const bottom = document.querySelector("#bottom");
+    const sidepane = document.querySelector(".sidepane");
+
+    sidepane.classList.toggle("expanded");
+    top.classList.toggle("top");
+    middle.classList.toggle("middle");
+    bottom.classList.toggle("bottom");
+})
